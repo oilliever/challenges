cf6fcacf051cb4758ea53eaa949e431077f5714d
oilliever <baduy.work@gmail.com>
2023-04-25 08:19:04 +0700

    done

--- src/App.js
+++ src/App.js
@@ -52,7 +52,6 @@ function App() {
             .then((req) => {
                 setData(req);
             });
-        console.log(API_URL);
     }, [API_URL]);
 
     const handleFindCity = () => {
